ce3b1bac65b39e87fcc716528b60aff27fa0699c
cuongngo2403 <87960567+cuongngo2403@users.noreply.github.com>
2021-07-26 12:11:48 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'he lu Hoanh!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: ' Hoanh còn thích mình nữa không 😙',
+    desc: 'Hoanh không nói là thích mình rùi đó nha ',
+    btnYes: 'vẫn còn thích cậu',
+    btnNo: 'Nằm mơ à,
+    question:'Trên thế giới hơn 7 tỉ người mà sao mj vẫn cứ yêu cậu <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
+    messDesc: 'i love you ',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
